9f88edd125c959b8fa79fd06cd1e915edfff68d0
devbini <cong8685@naver.com>
2024-08-12 02:39:27 +0900

    fix: Change Package folder structure 2024-08-12 Spring 패키지 구조 변경 (빈 설정) App. 어노테이션 수정

--- Back/src/main/java/com/wpu/demo/WpuApplication.java
+++ Back/src/main/java/com/wpu/demo/WpuApplication.java
@@ -3,9 +3,7 @@ package com.wpu.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class WpuApplication {
 
