26cdb7e0363233d2ecb35f4e2da55ccb9bca068b
devbini <cong8685@naver.com>
2024-08-13 09:01:11 +0900

    hotfix: Fixed CORS Config 2024-08-13 레지스트리 매핑 경로를 구체화

--- Back/src/main/java/com/wpu/demo/config/Config.java
+++ Back/src/main/java/com/wpu/demo/config/Config.java
@@ -9,7 +9,7 @@ public class Config implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/api/**")
                 .allowedOrigins("http://kwpu.co.kr")
                 .allowedMethods("GET")
                 .allowedHeaders("*")
